8506a5ab5a792e1b402b3d5d85cd68d45f8ba29a
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-09-06 22:18:05 +0530

    added js task

--- task/38.js
+++ task/38.js
@@ -5,15 +5,77 @@
 // 1 0 1 0
 // 1 0 1 0 1
 
-let num = 5;
+let i;
+let j;
+let n = 1;
 
-for(let i = 0; i<=num; i++){
-    for(let j = 0; j <=i;j++){
-
-        if(j%2 ==0){
-            document.write(1+"<br>");
-        }else{
-            document.write(0);
+for (i = 1; i <= 5; i++) {
+    for (j = 1; j <= i; j++) {
+        if (j % 2 != 0) {
+            document.write("1 ");
+        } else {
+            document.write("0 ");
         }
     }
+    document.write("<br>");
 }
+
+
+// A
+// B C
+// D E F
+// G H I J
+// K L M N O
+
+let a;
+let b;
+var k = 65;
+var c = 5;
+
+for (a = 1; a <= c; k++) {
+    for (b = 1; b <= a; b++, k++) {
+        document.write(String.fromCharCode(k) + " ");
+    }
+
+    document.write("<br>");
+
+}
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+
+let p = prompt("Enter the height : ");
+     p = Number(n);
+    let string = "";
+    let count = 1;
+
+    for (let d = 1; d <= p; d++) {
+        for (let j = 1; j <= d; j++) {
+            string += count;
+            count++;
+        }
+        string += "<br>";
+    }
+
+    document.write(string + "&nbsp;");
+
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+let r = 5;
+
+    for (let o = 0; o < r; o++) {
+
+        for (let m = 0; m <= i; m++) {
+            document.write("* ");
+        }
+        document.write("<br>");
+    }
